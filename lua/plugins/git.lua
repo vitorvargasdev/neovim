4c04b2d7e5db71e78bb41eab4909eca84279c2b1
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-04-27 05:37:02 -0300

    refactor: update git keymap binding for blame functionality

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -8,7 +8,7 @@ return {
       require("gitsigns").setup()
 
       vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns blame_line<CR>", {})
     end
   }
 }
