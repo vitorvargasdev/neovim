8450bcc019718f7e3b2a05f66a89bf2f06898522
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-07-27 20:04:55 -0300

    refactor(none-ls): update formatter configuration
    - Replace prettier with prettierd for better performance
    - Add conditional loading based on .prettierrc.js presence
    - Switch from eslint to eslint_d diagnostics
    - Add ESLint fix-all keymap on <leader>gc
    - Improve formatting with tab-based indentation

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,22 +1,28 @@
 return {
-  "nvimtools/none-ls.nvim",
-  dependencies = {
-    "nvimtools/none-ls-extras.nvim",
-  },
-  config = function()
-    local null_ls = require("null-ls")
+	"nvimtools/none-ls.nvim",
+	dependencies = {
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      sources = {
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.diagnostics.golangci_lint,
-        require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
-        -- null_ls.builtins.diagnostics.phpcs,
-        null_ls.builtins.diagnostics.phpstan,
-      },
-    })
+		null_ls.setup({
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.prettier,
+				null_ls.builtins.formatting.prettierd.with({
+					condition = function(utils)
+						return utils.has_file({ ".prettierrc.js" })
+					end,
+				}),
+				null_ls.builtins.diagnostics.golangci_lint,
+				require("none-ls.diagnostics.eslint_d"), -- requires none-ls-extras.nvim
+				-- null_ls.builtins.diagnostics.phpcs,
+				null_ls.builtins.diagnostics.phpstan,
+			},
+		})
 
-    vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-  end,
+		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		vim.keymap.set("n", "<leader>gc", ":EslintFixAll<CR>")
+	end,
 }
