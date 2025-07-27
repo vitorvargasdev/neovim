7bdc874c0b658d30d9ec6535dff565230011f95d
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-07-27 19:59:42 -0300

    feat(lsp): enhance Vue.js and TypeScript language server configuration
    - Configure Volar for Vue.js files with proper TypeScript SDK path
    - Configure vtsls for TypeScript/JavaScript files excluding .vue files
    - Add CSS and Tailwind CSS language servers
    - Add ESLint language server support (eslint and eslint_d)
    - Add new LSP keymaps: gr for references, <leader>rn for rename
    - Improve server initialization with proper filetypes and root patterns

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -17,10 +17,14 @@ return {
           "gopls",
           "golangci-lint",
           "typescript-language-server",
-          "vue-language-server",
+          "vue-language-server", -- This installs Volar
           "intelephense",
           "php-cs-fixer",
           "terraform-ls",
+          "css-lsp",
+          "tailwindcss-language-server",
+          "eslint_d",
+          "eslint-lsp",
         },
       })
     end,
@@ -30,34 +34,75 @@ return {
     lazy = false,
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
-
       local lspconfig = require("lspconfig")
 
+      -- Volar configuration for Vue.js
+      lspconfig.volar.setup({
+        capabilities = capabilities,
+        filetypes = { "vue" }, -- Only handle Vue files for better compatibility
+        init_options = {
+          vue = {
+            hybridMode = false, -- Keep false for better file completion and imports
+          },
+          typescript = {
+            tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+          },
+        },
+      })
+
+      -- TypeScript/JavaScript configuration
+      lspconfig.vtsls.setup({
+        capabilities = capabilities,
+        filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact" },
+        -- Exclude .vue files to let Volar handle them completely
+        root_dir = lspconfig.util.root_pattern("package.json", "tsconfig.json", "jsconfig.json", ".git"),
+      })
+
+      -- Lua LSP
       lspconfig.lua_ls.setup({
         capabilities = capabilities,
       })
 
-      lspconfig.vtsls.setup({
-        capabilities = capabilities,
-      })
-
+      -- Go LSP
       lspconfig.gopls.setup({
         capabilities = capabilities,
       })
 
+      -- PHP LSP
       lspconfig.intelephense.setup({
         capabilities = capabilities,
       })
 
+      -- Terraform LSP
       lspconfig.terraformls.setup({
         capabilities = capabilities,
       })
 
+      -- CSS LSP
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+
+      -- Tailwind CSS LSP
+      lspconfig.tailwindcss.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.eslint_d.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+      })
+
+      -- Key mappings
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "gh", vim.diagnostic.open_float, {})
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, {})
     end,
   },
 }
