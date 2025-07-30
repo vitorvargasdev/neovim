43481459d1b59e707f87b98b5983be44f2bda3b6
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-07-30 01:54:04 -0300

    refactor(plugins): replace neoscroll with smear-cursor config

--- lua/plugins/smooth.lua
+++ lua/plugins/smooth.lua
@@ -1,12 +1,11 @@
 return {
   {
     "sphamba/smear-cursor.nvim",
-    opts = {},
-  },
-  {
-    "karb94/neoscroll.nvim",
-    config = function()
-      require("neoscroll").setup({})
-    end,
+    opts = {
+      -- Smear cursor when switching buffers or windows.
+      smear_between_buffers = true,
+      -- Smear cursor when switching windows.
+      smear_between_wins = true,
+    },
   },
 }
