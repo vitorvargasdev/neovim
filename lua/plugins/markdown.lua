a64cb0a4a69514e3c662941b621895e8d8b6de23
vitorvargasdev <50197635+vitorvargasdev@users.noreply.github.com>
2025-07-30 09:07:58 -0300

    feat(plugins): replace render-markdown with obsidian.nvim

--- lua/plugins/markdown.lua
+++ lua/plugins/markdown.lua
@@ -1,11 +1,40 @@
 return {
 	{
-		"MeanderingProgrammer/render-markdown.nvim",
-		dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
-		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
-		-- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
-		---@module 'render-markdown'
-		---@type render.md.UserConfig
-		opts = {},
+		"obsidian-nvim/obsidian.nvim",
+		version = "*",
+		lazy = true,
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		config = function()
+			require("obsidian").setup({
+
+			daily_notes = {
+				folder = "daily",
+				date_format = "%Y-%m-%d",
+				alias_format = "%B %-d, %Y",
+			},
+
+			mappings = {
+				["gf"] = {
+					action = function()
+						return require("obsidian").util.gf_passthrough()
+					end,
+					opts = { noremap = false, expr = true, buffer = true },
+				},
+			},
+
+			ui = {
+				enable = true,
+				update_debounce = 200,
+				checkboxes = {
+					[" "] = { char = "☐", hl_group = "ObsidianTodo" },
+					["x"] = { char = "✔", hl_group = "ObsidianDone" },
+					[">"] = { char = "", hl_group = "ObsidianRightArrow" },
+					["~"] = { char = "󰰱", hl_group = "ObsidianTilde" },
+				},
+			},
+			})
+		end,
 	},
 }
